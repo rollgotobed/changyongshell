bb3dc7d18a541756a13f053f9a237e0c512197c1
rollgotobed <35453778+rollgotobed@users.noreply.github.com>
2022-03-13 11:11:49 +0800

    Create openssl_install.sh

---
+++ openssl_install.sh
@@ -0,0 +1,27 @@
+# 20220313 更新
+
+# 简单自用，不保证所有人可用
+# 环境 centos 7.9
+
+# openssl-1.1.1m.tar.gz
+
+wget https://www.openssl.org/source/openssl-1.1.1m.tar.gz
+tar -xzvf openssl-1.1.1m.tar.gz
+cd  openssl-1.1.1m
+./config --prefix=/usr/local/openssl
+./config -t
+make
+make install
+./config shared --prefix=/usr/local/openssl
+make clean
+make
+make install
+cd /usr/local
+ldd /usr/local/openssl/bin/openssl
+echo "/usr/local/openssl/lib" >> /etc/ld.so.conf
+ldconfig /etc/ld.so.conf
+mv /usr/bin/openssl /usr/bin/openssl.bk
+cp /usr/local/openssl/bin/openssl /usr/bin/openssl
+
+which openssl
+openssl version
